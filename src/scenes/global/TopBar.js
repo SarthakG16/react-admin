b314c6faaf5b6a3c75db0fcbc290ac0ca4869d11
SarthCoder <sarthakganoorkar@gmail.com>
2023-01-05 17:30:32 +0800

    Added Dark Light Theme Switch

---
+++ src/scenes/global/TopBar.js
@@ -0,0 +1,47 @@
+import { Box, IconButton, useTheme, InputBase } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext } from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const TopBar = () => {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2} maxHeight="8vh">
+      {/* Search Bar */}
+      <Box display="flex" borderRadius={"3px"}>
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Sarth" />
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default TopBar;
